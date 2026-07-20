2d25ff62d72ef1a226c7bc95d7d9ee74d1765492
Нелюбин Семён <nelubinsemen@gmail.com>
2026-07-20 21:14:28 +0500

    fetat: Добавлена поддержка расчёта размера директории без рекурсии

--- path_size.go
+++ path_size.go
@@ -7,10 +7,31 @@ import (
 )
 
 func GetPathSize(path string, _, _, _ bool) (string, error) {
-	inf, err := os.Lstat(path)
+
+	inf, err := os.Stat(path)
 	if err != nil {
 		return "", err
 	}
+	var size int64 = 0
+	if inf.IsDir() {
+		files, err := os.ReadDir(path)
+		if err != nil {
+			return "", err
+		}
+		for _, entry := range files {
+			if entry.IsDir() {
+				// TODO: recurcive walk
+			} else {
+				eInf, err := entry.Info()
+				if err != nil {
+					continue
+				}
+				size += eInf.Size()
+			}
+		}
+	} else {
+		size = inf.Size()
+	}
 
-	return fmt.Sprintf("%sB\t%s", strconv.FormatInt(inf.Size(), 10), path), nil
+	return fmt.Sprintf("%sB\t%s", strconv.FormatInt(size, 10), path), nil
 }
