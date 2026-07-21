774875ca3a77a2cf4b77baca992e3cb87f6c56df
Нелюбин Семён <nelubinsemen@gmail.com>
2026-07-21 22:52:16 +0500

    fix: Исправлен порядок параметров в тестах

--- path_size_test.go
+++ path_size_test.go
@@ -11,34 +11,34 @@ import (
 func TestExisitingFileSize(t *testing.T) {
 	path := "testdata/hello.txt"
 	res, err := GetPathSize(path, true, true, true)
-	require.Equal(t, err, nil)
-	require.Equal(t, res, fmt.Sprintf("5B\t%s", path))
+	require.Equal(t, nil, err)
+	require.Equal(t, fmt.Sprintf("5B\t%s", path), res)
 }
 
 func TestNonExistentFileSize(t *testing.T) {
 	path := "testdata/unknown.txt"
 	res, err := GetPathSize(path, true, true, true)
-	require.NotEqual(t, err, nil)
-	require.Equal(t, res, "")
+	require.NotEqual(t, nil, err)
+	require.Equal(t, "", res)
 }
 
 func TestDirFileSize(t *testing.T) {
 	path := "testdata"
 	res, err := GetPathSize(path, false, false, true)
-	require.Equal(t, err, nil)
-	require.Equal(t, res, fmt.Sprintf("100011B\t%s", path))
+	require.Equal(t, nil, err)
+	require.Equal(t, fmt.Sprintf("100011B\t%s", path), res)
 }
 
 func TestSizeHumanize(t *testing.T) {
 	var base float64 = 1024
-	require.Equal(t, FormatSize(10, true), "10B")
-	require.Equal(t, FormatSize(int64(base), true), "1KB")
-	require.Equal(t, FormatSize(int64(base*base), true), "1MB")
-	require.Equal(t, FormatSize(1234567, true), "1.2MB")
-	require.Equal(t, FormatSize(int64(base*base*base), true), "1GB")
-	require.Equal(t, FormatSize(int64(math.Pow(base, 4)), true), "1TB")
-	require.Equal(t, FormatSize(int64(math.Pow(base, 5)), true), "1PB")
-	require.Equal(t, FormatSize(int64(math.Pow(base, 6)), true), "1EB")
+	require.Equal(t, "10B", FormatSize(10, true))
+	require.Equal(t, "1KB", FormatSize(int64(base), true))
+	require.Equal(t, "1MB", FormatSize(int64(base*base), true))
+	require.Equal(t, "1.2MB", FormatSize(1234567, true))
+	require.Equal(t, "1GB", FormatSize(int64(base*base*base), true))
+	require.Equal(t, "1TB", FormatSize(int64(math.Pow(base, 4)), true))
+	require.Equal(t, "1PB", FormatSize(int64(math.Pow(base, 5)), true))
+	require.Equal(t, "1EB", FormatSize(int64(math.Pow(base, 6)), true))
 }
 
 func TestHiddenCounting(t *testing.T) {
