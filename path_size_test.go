9e9b5acee4eba3acf2c1bf6ef52cb2f4ed2fef7b
Нелюбин Семён <nelubinsemen@gmail.com>
2026-07-22 23:07:45 +0500

    test: Добавлены тесты пустой директории и пустого файла

--- path_size_test.go
+++ path_size_test.go
@@ -1,6 +1,8 @@
 package code
 
 import (
+	"math"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -20,9 +22,49 @@ func TestNonExistentFileSize(t *testing.T) {
 	require.Equal(t, "", res)
 }
 
+func TestEmptyFileSize(t *testing.T) {
+	path := "testdata/empty.txt"
+	res, err := GetPathSize(path, true, true, true)
+	require.Equal(t, nil, err)
+	require.Equal(t, "0B", res)
+}
+
 func TestDirFileSize(t *testing.T) {
 	path := "testdata"
 	res, err := GetPathSize(path, false, false, true)
 	require.Equal(t, nil, err)
 	require.Equal(t, "100011B", res)
+}
+
+func TestEmptyDirFileSize(t *testing.T) {
+	path := "testdata/emptydir"
+	mkdirErr := os.MkdirAll(path, os.ModePerm)
+	require.Equal(t, nil, mkdirErr)
+	res, err := GetPathSize(path, false, false, true)
+	require.Equal(t, nil, err)
+	require.Equal(t, "0B", res)
+}
+
+func TestHiddenCounting(t *testing.T) {
+	r1, _ := rawPathSize(".", false, false)
+	r2, _ := rawPathSize(".", false, false)
+	require.Equal(t, true, r1 >= r2)
+}
+
+func TestRecursiveWalk(t *testing.T) {
+	r1, _ := rawPathSize("testdata", true, false)
+	r2, _ := rawPathSize("testdata", false, false)
+	require.Equal(t, true, r1 >= r2)
+}
+
+func TestSizeHumanize(t *testing.T) {
+	var base float64 = 1024
+	require.Equal(t, "10B", formatSize(10, true))
+	require.Equal(t, "1.0KB", formatSize(int64(base), true))
+	require.Equal(t, "1.0MB", formatSize(int64(base*base), true))
+	require.Equal(t, "1.2MB", formatSize(1234567, true))
+	require.Equal(t, "1.0GB", formatSize(int64(base*base*base), true))
+	require.Equal(t, "1.0TB", formatSize(int64(math.Pow(base, 4)), true))
+	require.Equal(t, "1.0PB", formatSize(int64(math.Pow(base, 5)), true))
+	require.Equal(t, "1.0EB", formatSize(int64(math.Pow(base, 6)), true))
 }
