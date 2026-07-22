86a9f22eb3dcdb02e263617bbbceb23988308fa2
proGmtorg <pro@gmtorg.ru>
2026-07-22 08:26:28 +0500

    test: Тесты актуализированы

--- path_size_test.go
+++ path_size_test.go
@@ -1,7 +1,6 @@
 package code
 
 import (
-	"fmt"
 	"math"
 	"testing"
 
@@ -12,7 +11,7 @@ func TestExisitingFileSize(t *testing.T) {
 	path := "testdata/hello.txt"
 	res, err := GetPathSize(path, true, true, true)
 	require.Equal(t, nil, err)
-	require.Equal(t, fmt.Sprintf("5B\t%s", path), res)
+	require.Equal(t, "5B", res)
 }
 
 func TestNonExistentFileSize(t *testing.T) {
@@ -26,19 +25,19 @@ func TestDirFileSize(t *testing.T) {
 	path := "testdata"
 	res, err := GetPathSize(path, false, false, true)
 	require.Equal(t, nil, err)
-	require.Equal(t, fmt.Sprintf("100011B\t%s", path), res)
+	require.Equal(t, "100011B", res)
 }
 
 func TestSizeHumanize(t *testing.T) {
 	var base float64 = 1024
 	require.Equal(t, "10B", FormatSize(10, true))
-	require.Equal(t, "1KB", FormatSize(int64(base), true))
-	require.Equal(t, "1MB", FormatSize(int64(base*base), true))
+	require.Equal(t, "1.0KB", FormatSize(int64(base), true))
+	require.Equal(t, "1.0MB", FormatSize(int64(base*base), true))
 	require.Equal(t, "1.2MB", FormatSize(1234567, true))
-	require.Equal(t, "1GB", FormatSize(int64(base*base*base), true))
-	require.Equal(t, "1TB", FormatSize(int64(math.Pow(base, 4)), true))
-	require.Equal(t, "1PB", FormatSize(int64(math.Pow(base, 5)), true))
-	require.Equal(t, "1EB", FormatSize(int64(math.Pow(base, 6)), true))
+	require.Equal(t, "1.0GB", FormatSize(int64(base*base*base), true))
+	require.Equal(t, "1.0TB", FormatSize(int64(math.Pow(base, 4)), true))
+	require.Equal(t, "1.0PB", FormatSize(int64(math.Pow(base, 5)), true))
+	require.Equal(t, "1.0EB", FormatSize(int64(math.Pow(base, 6)), true))
 }
 
 func TestHiddenCounting(t *testing.T) {
